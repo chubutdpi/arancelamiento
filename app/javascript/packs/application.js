738c5abe1acc54962eeae5820ac5e4783e3876f4
Silvio Geoffroy <sageoffroy@gmail.com>
2021-12-06 12:49:44 -0300

    Instalación de Boostrap 5.1.3 webpack precompilado

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+
+//BOOTSTRAP
+import 'packs/bootstrap.bundle.min'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
